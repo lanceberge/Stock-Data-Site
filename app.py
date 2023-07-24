381030245d8606cad133c8b969fbba077468c4fa
lanceberge <bergeron.lance6@gmail.com>
2023-07-24 17:36:14 -0400

    Refactor- search fetches data and the ticker is passed to the backend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import sqlite3
 
 app = Flask(__name__)
@@ -7,17 +7,42 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/key_statistics')
 def key_statistics():
+    ticker = request.args.get('ticker')
+
+    query = f"SELECT * FROM test WHERE id={ticker}"
+    # TODO if data is too old or nonexistent, call API and replace data
+
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM test')
+    cursor.execute(query)
     data = cursor.fetchall()
     cursor.close()
     conn.close()
     return jsonify(data)
 
-# TODO create similar routes for other tabs
+
+@app.route('/balance_sheet')
+def balance_sheet():
+    pass
+
+
+@app.route('/earnings')
+def earnings():
+    pass
+
+
+@app.route('/cash_flow')
+def cash_flow():
+    pass
+
+
+@app.route('/insider_trading')
+def insider_trading():
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,5 +1,6 @@
 const mainTab = 'key_statistics'
-const fetchedData = {} // Object to store fetched data
+let fetchedData = {} // Object to store fetched data
+const tabs = ['key_statistics', 'balance_sheet', 'earnings', 'cash_flow', 'insider_trading']
 
 function switchTab (tabId) {
   const tabContents = document.querySelectorAll('.tab-content')
@@ -7,29 +8,30 @@ function switchTab (tabId) {
     tabContent.classList.remove('active')
   })
 
-  if (!fetchedData[tabId]) {
-    fetchDataForTab(tabId)
-  } else {
-    updateTabContent(tabId)
-  }
-
-  // const tabs = document.getElementsByClassName('tab-content')
   document.getElementById(tabId).classList.add('active')
+  updateTabContent(tabId)
 }
 
-function search () {
-  const query = document.getElementById('searchInput').value
-
-  switchTab(mainTab)
+async function search () {
   const tabNavigation = document.getElementById('tab-navigation')
   tabNavigation.style.display = 'block'
+
+  fetchedData = {}
+
+  const ticker = document.getElementById('search_ticker').value
+
+  await fetchDataForTab(mainTab, ticker)
+  switchTab(mainTab)
+
+  tabs.slice(1).forEach(tabId => {
+    fetchDataForTab(tabId, ticker)
+  })
 }
 
-async function fetchDataForTab (tabId) {
-  const response = await fetch(`/${tabId}`)
+async function fetchDataForTab (tabId, ticker) {
+  const response = await fetch(`/${tabId}?ticker=${ticker}`)
   const data = await response.json()
-  fetchedData[tabId] = data // Save fetched data to the object
-  updateTabContent(tabId)
+  fetchedData[tabId] = data
 }
 
 function updateTabContent (tabId) {
@@ -60,5 +62,12 @@ document.addEventListener('DOMContentLoaded', function () {
       const tabId = this.getAttribute('data-tab')
       switchTab(tabId)
     })
+  })
+
+  const searchInput = document.getElementById('search_ticker')
+  searchInput.addEventListener('keydown', function (event) {
+    if (event.keyCode === 13) {
+      search()
+    }
   })
 })
