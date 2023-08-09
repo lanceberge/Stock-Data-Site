fc9a9c0d96c4f9b9e9158777fccc1861a9acbe12
lanceberge <bergeron.lance6@gmail.com>
2023-08-09 17:11:05 -0400

    update number formatting with include_suffix argument

--- util/number_formatting.py
+++ util/number_formatting.py
@@ -1,19 +1,44 @@
 import math
 
 
-millnames = ['',' Thousand',' Million',' Billion',' Trillion']
+# TODO return thousands_base
+
+millnames = ["", "", "M", "B", "T"]
 
 
-def millify(n):
+def get_thousands_base(n):
     n = float(n)
-    millidx = max(0,min(len(millnames)-1,
-                        int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
 
-    return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
+    return max(0, int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3)))
+
+
+def millify(n, thousands_base=None, include_suffix=True):
+    n = float(n)
+
+    if not thousands_base:
+        thousands_base = get_thousands_base(n)
+
+    if thousands_base > 1:
+        n = n / 10 ** (3 * thousands_base)
+
+    # return the string with no decimals if the number doesn't have any
+    millnames_idx = min(thousands_base, len(millnames) - 1)
+
+    rstring = None
+    if n.is_integer():
+        rstring =  "{:,.0f}".format(n)
+
+    else:
+        rstring = "{:,.2f}".format(n)
+
+    if include_suffix:
+        return rstring.append(millnames[millnames_idx])
+
+    return rstring
 
 
 def percentify(n):
-    return "{:.2f}%".format(n*100)
+    return "{:.2f}%".format(n * 100)
 
 
 def two_decimals(n):
