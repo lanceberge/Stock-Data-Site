ead6aac05c208750e84b9322da3720f605a0aeb5
lanceberge <bergeron.lance6@gmail.com>
2023-09-07 15:00:02 -0400

    select from database function

--- util/database_management.py
+++ util/database_management.py
@@ -9,3 +9,22 @@ def insert_into_database(data, table_name):
     cursor.close()
     conn.close()
 
+
+def select_from_database(table_name, column_names, filters=None):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    query = "SELECT %s FROM %s" % (','.join(column_names), table_name)
+    if filters:
+        query += " " + filters
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+
+    result_list = []
+    for row in data:
+        row_dict = dict(zip(column_names, row))
+        result_list.append(row_dict)
+    
+    cursor.close()
+    conn.close()
+    return result_list
